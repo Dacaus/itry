5e4b4a8fe8690c82558a5aa0b947b02291c4022e
dankinder <2535262279@qq.com>
2026-01-17 11:42:31 +0000

    add

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,4 +1,5 @@
 apt-get update
 apt-get install -y clang-21 clangd-21 llvm-21
 apt-get install -y cmake git curl zstd libzstd-dev tree
+mkdir -p /root/.ssh
 ssh-keyscan github.com >> ~/.ssh/known_hosts
