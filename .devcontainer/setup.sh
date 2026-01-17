511d1535ca71851e09527ae0d9ff0ae751bfc729
dankinder <2535262279@qq.com>
2026-01-17 12:06:34 +0000

    add change

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,10 +2,13 @@ apt-get update
 apt-get install -y clang-21 
 apt-get install -y clangd-21 
 apt-get install -y llvm-21
+apt-get install -y llvm-21-dev
 apt-get install -y cmake 
 apt-get install -y git 
 apt-get install -y curl 
 apt-get install -y zstd 
 apt-get install -y libzstd-dev 
 apt-get install -y tree
+apt-get install -y libedit-dev
+apt-get install -y libcurl4-openssl-dev
 ssh-keyscan github.com >> ~/.ssh/known_hosts
